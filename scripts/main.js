36c0fa8a8133b4272faf59a94958b1aa64d56228
Joe <jlouis2k4@gmail.com>
2022-08-07 13:27:29 -0400

    Removed Default Username Setting

--- scripts/main.js
+++ scripts/main.js
@@ -23,12 +23,6 @@ function setUserName() {
   }
 }
 
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
 myButton.onclick = function() {
   setUserName();
 }
